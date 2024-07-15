52b8304697fc27b2e19759d9e26c8c1ac43d9163
blindcary <akiseera@gmail.com>
2024-07-15 13:33:43 +0300

    Create basic exercises.py

---
+++ basic exercises.py
@@ -0,0 +1,25 @@
+#3
+from datetime import datetime
+now = datetime.now()
+formatted_date_time=now.strftime("%d-%m-%Y %H:%M:%S")
+print("date:",formatted_date_time)
+
+#4
+import math
+def calculate_area_of_circle(radius):
+    area = math.pi * (radius ** 2)
+    return area
+def main():
+    radius = float(input("radius:"))
+    area = calculate_area_of_circle(radius)
+    print("radius:",radius,"area:",area)
+if __name__ == "__main__":
+    main()
+
+#5
+def revers_name():
+    first_name = input("Enter your first name:")
+    last_name = input("Enyer last name:")
+    print(last_name, "" ,first_name)
+if __name__ == "__main__":
+    revers_name()
